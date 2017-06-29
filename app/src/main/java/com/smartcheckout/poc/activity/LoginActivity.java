a76c98f2fd36e90a4956821595e49361f1be5feb
rahuldalal <rahul.v.dalal@gmail.com>
2017-06-28 23:06:27 -0700

    Completed upto store selection and showing initial cart

---
+++ app/src/main/java/com/smartcheckout/poc/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.smartcheckout.poc.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.smartcheckout.poc.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
